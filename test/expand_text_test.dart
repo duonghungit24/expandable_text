d64abdf9ff2752e47b9b587d9431cf4c167231a2
duonghung2412 <hungduong721@gmail.com>
2025-08-26 14:17:48 +0700

    update

--- test/expand_text_test.dart
+++ test/expand_text_test.dart
@@ -2,4 +2,11 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:expand_text/expand_text.dart';
 
-void main() {}
+void main() {
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+  });
+}
